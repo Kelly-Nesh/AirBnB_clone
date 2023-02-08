3100ca69d14894b8c50790e42b1ddc3375da105f
Kelly-Nesh <110617079+Kelly-Nesh@users.noreply.github.com>
2023-02-08 20:52:18 +0300

    added documentation for init
    added self method

--- models/base_model.py
+++ models/base_model.py
@@ -7,14 +7,20 @@ attributes/methods for other classes"""
 
 class BaseModel:
     def __init__(self):
+        """Initializes id, and created at when BaseModel is instantiated
+        
+        Args:
+            self: instance of BaseModel
+        """
         self.id = str(uuid.uuid4())
-        self.created_at =
-        self.updated_at = datetime.now()
-        # if first time update both created_at and updated_at
-        # if second time just update updated_at
+        self.created_at = datetime.now()
+        self.updated_at = ''
 
 
     def __str__(self):
         return "[()] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
