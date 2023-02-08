0380bcee5d08e46529a960bd6f4f051dc110fcfb
Kelly-Nesh <110617079+Kelly-Nesh@users.noreply.github.com>
2023-02-08 21:17:41 +0300

    Added docstring to BaseModel methods

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,6 @@ attributes/methods for other classes"""
 class BaseModel:
     def __init__(self):
         """Initializes id, and created at when BaseModel is instantiated
-        
-        Args:
-            self: instance of BaseModel
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
@@ -18,12 +15,15 @@ class BaseModel:
 
 
     def __str__(self):
+        """return string representation of the class name with the id and the dict"""
         return "[()] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
         dict_representation = self.__dict__.copy()
         dict_representation["__class__"] = self.__class__.__name__
         dict_representation["created_at"] = self.created_at.isoformat()
