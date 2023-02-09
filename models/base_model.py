083d6ea8da1a592009c5b05b0284878e59cfe3ec
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-02-09 11:23:07 +0300

    question 5 done

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from engine import storage
 """Defining a BaseModel Class that defines all common
 attributes/methods for other classes"""
 
@@ -19,6 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """return string representation of the class name with the id and the dict"""
@@ -27,6 +29,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
