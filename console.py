c559ef0403c9529ad7ec70058de78c10d78a3c02
Abraq <97287240+Abraq-SQA@users.noreply.github.com>
2023-02-09 18:32:59 +0100

    update console.py

--- console.py
+++ console.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 import cmd
-"""creating interactive console"""
 
+import models
+from models.base_model import BaseModel
+"""creating interactive console"""
 
 class HBNBCommand(cmd.Cmd):
     """Python interactive console"""
     prompt = "(hbnb) "
-
+    stoorage = models.storage
+    
+    def do_create(self, argv):
+        """Creates a new instance of BaseModel, saves it (to JSON file)
+        and prints the id"""
+        args = check_args(argv)
+        if args:
+    
     def do_quit(self, qt):
         """Quit command to exit the program"""
         return True
