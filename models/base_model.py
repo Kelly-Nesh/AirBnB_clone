498efb8fccf6903cce16465c8e41d94c954126d3
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-02-09 09:38:29 +0300

    question 4 done and rectified issues with updated_at dict key

--- models/base_model.py
+++ models/base_model.py
@@ -6,17 +6,23 @@ attributes/methods for other classes"""
 
 
 class BaseModel:
-    def __init__(self):
+    """BaseModel"""
+
+    def __init__(self, *args, **kwargs):
         """Initializes id, and created at when BaseModel is instantiated
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = ''
-
+        if kwargs:
+            self.id = kwargs['id']
+            self.created_at = datetime.fromisoformat(kwargs['created_at'])
+            self.updated_at = datetime.fromisoformat(kwargs['updated_at'])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """return string representation of the class name with the id and the dict"""
-        return "[()] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
@@ -27,5 +33,5 @@ class BaseModel:
         dict_representation = self.__dict__.copy()
         dict_representation["__class__"] = self.__class__.__name__
         dict_representation["created_at"] = self.created_at.isoformat()
-        dict_representation["updated-at"] = self.updated_at.isoformat()
+        dict_representation["updated_at"] = self.updated_at.isoformat()
         return dict_representation
