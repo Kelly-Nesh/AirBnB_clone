929ca0914a9f1e1d29a8b06460bb6bb44a68724a
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-02-11 12:09:34 +0300

    console.py update method not working

--- console.py
+++ console.py
@@ -71,14 +71,67 @@ class HBNBCommand(cmd.Cmd):
         rt = []
         if not class_all or class_all == 'BaseModel':
             all_saved = models.engine.storage.all()
-            for s in all_saved.copy().items():
-                str_insta = str(BaseModel(s))
-                rt.append(str_insta)
-            print(rt,'\n\n')
+            cp_saved = all_saved.values()
+            for itm in cp_saved:
+                id = itm['id']
+                updated_at = itm['updated_at']
+                created_at = itm['created_at']
+                old_instance = BaseModel(id=id, created_at=created_at, 
+                updated_at=updated_at)
+                rt.append(str(old_instance))
+            print(rt)
             del(rt)
         elif class_all != 'BaseModel':
                 print("** class doesn't exist **")
-
+    
+    def do_update(self, class_update):
+        """Updates an instance based on class name and id by adding or updating
+        attributes and saves the changes to json file"""
+        class_update = class_update.split(' ')
+        saved = models.engine.storage.all()
+        if not class_update[0]:
+            print("** class name missing **")
+        elif class_update[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(class_update) < 2:
+            print("** instance id missing **")
+        elif class_update[1] not in saved:
+            print("** no instance found **")
+        elif len(class_update) < 3:
+            print("** attribute name missing **")
+        elif len(class_update) < 4:
+            print("** value missing **")
+        else:
+            old_inst = saved[class_update[1]]
+            id = old_inst['id']
+            created_at = old_inst['created_at']
+            updated_at = old_inst['updated_at']
+            to_update = BaseModel(id=id, created_at=created_at,
+            updated_at=updated_at)
+            setattr(to_update, class_update[2], 
+                HBNBCommand.type_cast(class_update[3]))
+            to_update.save()
+            models.engine.storage.reload()
+    
+    @staticmethod
+    def type_cast(k):
+        try:
+            k=int(k)
+        except ValueError:
+            try:
+                k=float(k)
+            except ValueError:
+                try:
+                    k=str(k)
+                except ValueError:
+                    pass
+                else:
+                    return k
+            else:
+                return k
+        else:
+            return k
+    # HELP METHODS
     def help_show(self):
         print('\n'.join(["Takes two arguments: Class and Instance Id",
         "Usage: ``show BaseModel 79734628-ebca-4188-81fe-932c7a967b84``"]))
@@ -91,6 +144,8 @@ class HBNBCommand(cmd.Cmd):
         print('\n'.join(["prints all string representation of all instances",
 	"whether specified class or not", 
 	"Usage: ``all BaseModel`` or ``all``"]))
+
+    # OVERRIDEN METHODS
     def do_quit(self, qt):
         """Quit command to exit the program"""
         return True
@@ -104,3 +159,4 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
