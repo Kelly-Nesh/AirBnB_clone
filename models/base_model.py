a4bda97c575c1e49dd263842ce6e04a412d3b6dc
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-02-21 14:11:25 +0300

    updated base_model save method

--- models/base_model.py
+++ models/base_model.py
@@ -32,6 +32,7 @@ class BaseModel:
         """updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
