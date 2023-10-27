1399e2a7fa6bd24dbfb6d907e36ceec7d7f9a56f
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-27 09:56:49 +0300

    fixed syntax error in init and removed print debugger

--- console.py
+++ console.py
@@ -12,27 +12,30 @@ class HBNBCommand(cmd.Cmd):
     class definition of  command interpreter:
     """
     prompt = "(hbnb) "
-    
+
     def do_create(self, args):
         if self.check_args("create", args):
             self.model = models.mdcls[args]()
             self.model.save()
             print(self.model.id)
+
     def do_show(self, args):
         if self.check_args("show", args):
             args = args.split(" ")
             key = args[0] + "." + args[1]
-            #models.storage.reload()
+            # models.storage.reload()
             print(str(models.storage.all().get(key,
-                "** no instance found **")))
+                                               "** no instance found **")))
+
     def do_destroy(self, args):
         if self.check_args("destroy", args):
             args = args.split(" ")
             key = args[0] + "." + args[1]
-            #models.storage.reload()
+            # models.storage.reload()
             models.storage.all().pop(key,
-                "** no instance found **")
+                                     "** no instance found **")
             models.storage.save()
+
     def do_all(self, args):
         all_models = models.storage.all()
         strings = []
@@ -63,9 +66,6 @@ class HBNBCommand(cmd.Cmd):
             model_update = models.storage.all()[key]
             model_update.__dict__[args[2]] = self.type_cast(args[3])
             models.storage.save()
-            print(model_update.__dict__, models.storage.all()[key], sep="\n\n")
-
-
 
     @staticmethod
     def type_cast(k):
@@ -87,7 +87,6 @@ class HBNBCommand(cmd.Cmd):
         else:
             return k
 
-
     @staticmethod
     def check_args(caller, args):
         if args:
@@ -101,31 +100,35 @@ class HBNBCommand(cmd.Cmd):
         else:
             return True
 
-
     def do_EOF(self, c):
         print()
-        return True 
+        return True
+
     def do_quit(self, c):
         return True
+
     def emptyline(self):
         return
 
     def help_quit(self):
         print("""Quit the command interpreter""")
+
     def help_create(self):
         message = """
         Creates a new instance of BaseModel, saves it (to the JSON file)
-        and prints the id. 
+        and prints the id.
         Usage: `create BaseModel`
         """
         print(message)
+
     def help_show(self):
         message = """
         Prints the string representation of an instance based
-        on the class name and id. 
+        on the class name and id.
         Usage: `show BaseModel 1234-1234-1234`
         """
         print(message)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 from models import user, amenity, state, place, review, city
 
 mdcls = {"BaseModel": BaseModel, "User": user.User,
-        "Amenity": amenity.Amenity, "State", state.State,
+        "Amenity": amenity.Amenity, "State": state.State,
         "Place": place.Place, "Review": review.Review,
         "City": city.City}
 storage = FileStorage()
