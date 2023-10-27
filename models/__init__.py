14d26efc0c5a165acdb22c71694347a5d212cbfe
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-27 09:50:19 +0300

    added the classes to models.init

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,11 @@
 
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-from models import user
+from models import user, amenity, state, place, review, city
 
-mdcls = {"BaseModel": BaseModel, "User": user.User}
+mdcls = {"BaseModel": BaseModel, "User": user.User,
+        "Amenity": amenity.Amenity, "State", state.State,
+        "Place": place.Place, "Review": review.Review,
+        "City": city.City}
 storage = FileStorage()
 storage.reload()
